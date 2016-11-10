4d82c8efcfd398fe46c81c99a7aa14950406eb2a
Juarez Rudsatz <juarezr@gmail.com>
2016-11-10 14:10:50 -0200

    ui-grid: wip1: initial implementation

--- src/can-semantic-ui.js
+++ src/can-semantic-ui.js
@@ -1,3 +1,9 @@
+import './ui-grid/ui-grid';
+import './ui-grid/ui-column';
+import './ui-grid/ui-row';
+
+import './common.less!';
+
 export default function() {
   return `This is the can-semantic-ui plugin`;
 }
